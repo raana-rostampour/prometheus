3463a7282b8456bfb40801539ff1f9c12f76246e
raana <raana@smartech.ir>
2023-07-27 20:02:00 +0330

    update description of metric

--- script.sh
+++ script.sh
@@ -7,10 +7,10 @@ echo action
 read action
 echo region
 read region
-text_file_collector_path=/opt/prometheus/node_exporter/textfile_collector/cloud_request_"$action"_"$region".prom
+text_file_collector_path=/opt/prometheus/node_exporter/textfile_collector/request_"$action"_"$region".prom
 
 ### important lines for sending TYPE
-echo -e "# HELP cloud_request Last success in seconds.\n# TYPE cloud_request counter" > $text_file_collector_path
+echo -e "# HELP request total number of success and fail requests .\n# TYPE request counter" > $text_file_collector_path
 
 echo  "request{service=\"cloud\", region=\"$region\",status=\"failed\",action=\"$action\"} $request_failure_count" >> $text_file_collector_path
 echo  "request{service=\"cloud\" , region=\"$region\",status=\"successed\",action=\"$action\"} $request_success_count" >> $text_file_collector_path
